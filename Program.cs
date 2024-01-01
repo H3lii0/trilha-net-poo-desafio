83ce2cb187c0a9ff368f7a7092f5bfb9e58f4fab
H3lii0 <helio.miranda.n@gmail.com>
2024-01-01 11:24:47 -0300

    Desafio conluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modeloa 1", imei: "222222222222", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modeloa 1", imei: "222222222222", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
